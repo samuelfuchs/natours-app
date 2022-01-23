4d5f1399a7c2110caddc7912c1e724ca1d7b001d
samuelfuchs <fuchsgerson@gmail.com>
2022-01-23 18:15:14 -0300

    Handling POST Requests

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 
 const app = express();
 
+// MIDDLEWARE - modify the incomming data
+app.use(express.json());
+
 // app.get("/", (req, res) => {
 //   res
 //     .status(200)
@@ -28,6 +31,29 @@ app.get("/api/v1/tours", (req, res) => {
       tours: tours,
     },
   });
+});
+
+// Create a new tour - it's the SAME route as GET request
+app.post("/api/v1/tours", (req, res) => {
+  //   console.log(req.body);
+  // 1st figure out the ID of object
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      // STATUS 201 = created OK
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
