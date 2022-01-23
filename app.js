5340073a46dcb388472aa0e1db4dd374c8835d7e
samuelfuchs <fuchsgerson@gmail.com>
2022-01-23 18:01:31 -0300

    Starting Our API: Handling GET Requests

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello rom the server side!", app: "Natours" });
-});
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello rom the server side!", app: "Natours" });
+// });
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
+// app.post("/", (req, res) => {
+//   res.send("You can post to this endpoint...");
+// });
+
+// Read data before routing!!
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    // It's good practice to send the total amount of results, but not NECESSARY
+    results: tours.length,
+    data: {
+      // We could write only 'tours', without ': tours'
+      tours: tours,
+    },
+  });
 });
 
 const port = 3000;
