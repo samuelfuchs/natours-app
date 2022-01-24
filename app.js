b7950ce48a46a100d711f8956fd81ef2591bc3eb
samuelfuchs <fuchsgerson@gmail.com>
2022-01-24 10:39:47 -0300

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("/api/v1/tours/:id", (req, res) => {
   console.log(req.params);
 
   // convert id to string
-  // when multiplying by 1 it turns it into a string!!!
+  // when multiplying by 1 it turns it into a number!!!
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
@@ -79,6 +79,22 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour here...>",
+    },
+  });
 });
 
 const port = 3000;
