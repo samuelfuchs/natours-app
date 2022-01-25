6a2546fa4e094d5d28ee137c7c76a3ad7943feac
samuelfuchs <fuchsgerson@gmail.com>
2022-01-25 03:24:16 -0300

    Param Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -23,14 +34,6 @@ exports.getTour = (req, res) => {
   // when multiplying by 1 it turns it into a number!!!
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-
-  // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
 
   res.status(200).json({
     status: "success",
@@ -63,13 +66,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
   res.status(200).json({
     status: "success",
     data: {
@@ -79,12 +75,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
   // 204 - Data no longer exists
   res.status(204).json({
     status: "success",
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,8 @@ const tourController = require("../controllers/tourController");
 
 const router = express.Router();
 
+router.param("id", tourController.checkID);
+
 router
   .route("/")
   .get(tourController.getAllTours)
