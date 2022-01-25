e67d47cbd6405dd700db8386ae25906d7f018976
samuelfuchs <fuchsgerson@gmail.com>
2022-01-25 19:15:46 -0300

    Chaining Multiple Middleware Functions

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,7 +10,7 @@ router.param("id", tourController.checkID);
 router
   .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route("/:id")
