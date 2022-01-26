7e8f0de306f72da16e32c8029ace34f29cdc0e11
samuelfuchs <fuchsgerson@gmail.com>
2022-01-26 20:21:52 -0300

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -36,6 +36,20 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR!!!!', err);
+  });
+
 // 4) START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
