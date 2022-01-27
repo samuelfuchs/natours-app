8b5a4ecf0a44e4081dcf784eca582f178dd835f5
samuelfuchs <fuchsgerson@gmail.com>
2022-01-27 11:57:39 -0300

    Reading Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,32 +28,47 @@ const Tour = require('./../models/tourModel');
 //   next();
 // };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours: tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
-exports.getTour = (req, res) => {
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+
   // assigns automatically the requesto to ":id"
-  console.log(req.params);
 
   // convert id to string
   // when multiplying by 1 it turns it into a number!!!
   // const id = req.params.id * 1;
   // const tour = tours.find((el) => el.id === id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
 };
 
 exports.createTour = async (req, res) => {
