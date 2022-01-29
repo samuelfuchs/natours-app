940696ce8601718a5849f7055c482b0b2a2bdc89
samuelfuchs <fuchsgerson@gmail.com>
2022-01-29 09:28:00 -0300

    Updating Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -104,13 +104,25 @@ exports.createTour = async (req, res) => {
   // );
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!!',
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
