5c1507df77ca55776efa5ef900b76b801f7624ef
samuelfuchs <fuchsgerson@gmail.com>
2022-02-05 09:08:31 -0300

    Aggregation Middleware part1

--- models/tourModel.js
+++ models/tourModel.js
@@ -100,7 +100,14 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
   next();
 });
 
